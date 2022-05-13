28b3546d228a4561c400d6d5b52ec5becfa06a26
Ender4LionIsGamer <98265648+Ender4LionIsGamer@users.noreply.github.com>
2022-05-13 22:18:37 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt install wget unzip
+wget-o mc-server-on-heroku-template-SPIGOT.zip https://970-916e870b-c1b2-48d0-8e4c-4fce691d83d5.cs-asia-southeast1-ajrg.cloudshell.dev/files/download/?id=e26c663d-ff50-448c-8091-5d0893ae1f4e
+unzip mc-server-on-heroku-template-SPIGOT.zip
+rm -rf mc-server-on-heroku-template-SPIGOT.zip
