06d28b950ab2b05b64b8c44384ab31e269c03b74
Ender4LionIsGamer <98265648+Ender4LionIsGamer@users.noreply.github.com>
2022-05-14 10:19:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-wget https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
-mv spigot-1.8.8-R0.1-SNAPSHOT-latest.jar  minecraft_server.1.8.8.jar
+sudo apt install default-jdk
+clear
 java -Xms6G -Xmx6G -XX:+UseG1GC -jar minecraft_server.1.8.8.jar nogui
+clear 
+exit
