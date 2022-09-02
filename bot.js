002fbbca3f6369cf9ed0b9e766159c2f60869c1d
Ender4LionIsGamer <98265648+Ender4LionIsGamer@users.noreply.github.com>
2022-09-02 05:14:53 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
+    host: "kchcraft.aternos.me",
     port: "25565",
     username: "Test",
-    version: false
+    version: 1.8.9
     })
     bot.on('login', function() {
       bot.chat('/reginster 123123123 123123123')
